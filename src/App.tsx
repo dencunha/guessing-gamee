f95f8b2109609c34c836eb3ecc3af6aaacef2dfc
Blenda Cunha <blenda.cunha13@gmail.com>
2025-10-03 19:04:38 -0300

    Letter + Main

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import styles from "./app.module.css"
 
 import { Header } from "./components/Header"
 import { Tip } from "./components/Tip"
+import { Letter } from "./components/Letter"
 
 export default function App() {
 
@@ -17,7 +18,15 @@ export default function App() {
 
       <Tip tip="Uma das linguagens de programação mais utilizadas"/>
 
-      
+      <div className={styles.word}>
+      <Letter value="R" />
+      <Letter value="e" />
+      <Letter value="a" />
+      <Letter value="c" />
+
+      </div>
+
+
       </main>
     </div>
   )
